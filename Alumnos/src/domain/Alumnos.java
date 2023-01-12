5c7b2c868e2f6a67103587cb2dd8f90c76572a12
Reaper Diper <105932267+Yormenxx@users.noreply.github.com>
2023-01-12 08:00:19 -0500

    Add files via upload

---
+++ Alumnos/src/domain/Alumnos.java
@@ -0,0 +1,58 @@
+
+package domain;
+
+public class Alumnos {
+
+    private int id;
+    
+    private String nombre;
+    
+    private String apellido;
+    
+    
+    public Alumnos(int id, String nombre, String apellido){
+        
+        this.id = id;
+        
+        this.nombre = nombre;
+        
+        this.apellido = apellido;
+        
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumnos{" + "id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+
+    
+
+
+
+    
+}
---
+++ Alumnos/src/test/testAlumnos.java
@@ -0,0 +1,21 @@
+
+package test;
+
+import domain.Alumnos;
+
+
+public class testAlumnos {
+    
+    public static void main(String[] args) {
+        
+        
+        Alumnos alumno1 = new Alumnos(1,"juan","ortiz");
+        
+        
+        System.out.println("el alumno 1 es:" + alumno1);
+        
+        
+        
+    }
+    
+}
